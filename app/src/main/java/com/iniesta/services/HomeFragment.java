33a8b16f13d305a559ec757726a926d80c1fd3b8
Shubham Tripathi <tripathi.shubham509@gmail.com>
2019-06-12 15:09:34 +0530

    added horizontal recycler view for offers

--- app/src/main/java/com/iniesta/services/HomeFragment.java
+++ app/src/main/java/com/iniesta/services/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,21 +15,32 @@ import android.view.ViewGroup;
 
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
 
     MaterialSearchView searchView;
+    RecyclerView offersRecyclerView;
+    private ArrayList<Integer> imagesList = new ArrayList<>();
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        addImages();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
-        searchView = (MaterialSearchView) view.findViewById(R.id.search_view);
 
+        offersRecyclerView = view.findViewById(R.id.offersRecycler);
+        OffersRecyclerAdapter recyclerAdapter = new OffersRecyclerAdapter(getContext(),imagesList);
+        offersRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        offersRecyclerView.setAdapter(recyclerAdapter);
+
+        searchView = (MaterialSearchView) view.findViewById(R.id.search_view);
         searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -65,4 +78,18 @@ public class HomeFragment extends Fragment {
         searchView.setMenuItem(item);
 
     }
+
+    public void addImages(){
+        imagesList.add(R.drawable.salonicon);
+        imagesList.add(R.drawable.salonicon);
+        imagesList.add(R.drawable.salonicon);
+        imagesList.add(R.drawable.salonicon);
+        imagesList.add(R.drawable.salonicon);
+        imagesList.add(R.drawable.salonicon);
+        imagesList.add(R.drawable.salonicon);
+        imagesList.add(R.drawable.salonicon);
+        imagesList.add(R.drawable.salonicon);
+        imagesList.add(R.drawable.salonicon);
+    }
+
 }
