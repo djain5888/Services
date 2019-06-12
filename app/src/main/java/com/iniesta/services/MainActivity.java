201283c163c187e862ccec121d9e6afef446361f
Ranger12021999 <amanksharma123a@gmail.com>
2019-06-12 16:06:06 +0530

    Dialog

--- app/src/main/java/com/iniesta/services/MainActivity.java
+++ app/src/main/java/com/iniesta/services/MainActivity.java
@@ -1,13 +1,25 @@
 package com.iniesta.services;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    final Context context = this;
+    SharedPreferences s1;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,12 +57,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences prefs = getSharedPreferences("hello", MODE_PRIVATE);
+        int restoredText = prefs.getInt("idname", 0);
+
         BottomNavigationView navigationView = findViewById(R.id.navigation);
         navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         loadFragment(new HomeFragment());
 
+
+        if (restoredText != 12) {
+            final Dialog dialog = new Dialog(context);
+
+
+            dialog.setContentView(R.layout.custom);
+            dialog.setTitle("Title...");
+
+
+            // set the custom dialog components - text, image and button
+            TextView text = (TextView) dialog.findViewById(R.id.text);
+            text.setText("Android custom dialog ");
+            ImageView image = (ImageView) dialog.findViewById(R.id.image);
+            ImageView cross = dialog.findViewById(R.id.image1);
+            cross.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+//        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+            // if button is clicked, close the custom dialog
+//        dialogButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                dialog.dismiss();
+//            }
+//        });
+
+            dialog.show();
+            SharedPreferences.Editor editor = getSharedPreferences("hello", MODE_PRIVATE).edit();
+            editor.putInt("idName", 12);
+            editor.apply();
+        }
     }
+
+
+
+
 
     private boolean loadFragment(Fragment fragment){
 
